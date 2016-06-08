a2a5bee0b8dbf3140456406908fcd2e46eee2ee8
Eric <theoriginalsine@gmail.com>
2016-06-08 11:05:16 -0400

    Added a date printout to the ExecuteTest test class, and removed printouts from the Execute class. Cleaned up misc lines of code in Execute.

--- src/test/ExecuteTest.java
+++ src/test/ExecuteTest.java
@@ -1,17 +1,19 @@
+package test;
 
-
-import java.io.FileReader;
 import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
 
+import main.ProjectType;
 import main.WebScrapper;
 import org.json.JSONException;
 import org.json.simple.JSONArray;
 
 import main.Execute;
+import main.Role;
 
 
 /**
@@ -41,8 +43,37 @@ public class ExecuteTest {
         Date endDate = endCalenderDate.getTime();
 
         execute.populateForcastDyanamic(startDate, endDate);
+        makeList(execute.getRoleMap());
+
     }
 
+    public static void makeList(HashMap<String, Role> roleMap){
+        for (String roleName : roleMap.keySet()){
+            System.out.println("Role: " + roleName);
 
+            Role role = roleMap.get(roleName);
+            HashMap<String, ProjectType> projectTypeMap = role.getPmap();
 
+            for (String projectCode : projectTypeMap.keySet()){
+                System.out.println("    Project Code: " + projectCode);
+
+                HashMap<Date, Double> weekMapHours = projectTypeMap.get(projectCode).getWMap();
+
+                double employeeNumber = role.getEmp_Set().size();
+                HashMap<Date, Double> weekMapEmployees = projectTypeMap.get(projectCode).scaleMap(employeeNumber);
+
+                for (Date weekDate : weekMapHours.keySet()){
+                    double hours = weekMapHours.get(weekDate);
+                    double employeeHours = weekMapEmployees.get(weekDate);
+
+                    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+                    String forecastDate = sdf.format(weekDate);
+
+                    System.out.print("        Date: " + forecastDate);
+                    System.out.println(" Hours: " + hours + ", Employees: " + employeeHours);
+
+                }
+            }
+        }
+    }
 }
