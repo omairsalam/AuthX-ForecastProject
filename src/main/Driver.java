86fce2b1d488d3de679c7a788307953364f34627
Eric <theoriginalsine@gmail.com>
2016-06-08 16:06:25 -0400

    Implemented: - Driver class - Execute.Initialize() - GUIHandler (Placeholder)

---
+++ src/main/Driver.java
@@ -0,0 +1,44 @@
+package main;
+
+import org.jfree.chart.ChartFrame;
+import org.json.JSONException;
+import org.json.simple.JSONArray;
+import org.json.simple.parser.ParseException;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+
+/**
+ * Created by user on 6/8/16.
+ */
+public class Driver {
+
+
+    /**
+     * Sets the roleMap of this class
+     */
+    public static void main(String[] args) throws InterruptedException, ParseException, JSONException, IOException {
+
+        String downloadPath = args[0];
+        String username = args[1];
+        String password = args[2];
+
+        WebScrapper webScrapper = new WebScrapper(downloadPath, username, password);
+
+        JSONArray content = webScrapper.downloadJSONArray();
+
+        Execute execute = new Execute();
+
+        Date startDate = webScrapper.getStartDate();
+        HashMap<String, Role> roleMap = execute.initialize(startDate, content);
+
+        ArrayList<ChartFrame> chartFrames = GraphPlotter.getAllFrames(roleMap);
+
+        GUIHandler.makeGraphs(chartFrames);
+    }
+
+    //TODO: Modify end date
+    //TODO: modify employee number
+}
