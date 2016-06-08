2db08eec0f1cd48145a37b4037168c8d507c2a5e
Omair Alam <alam@USER-4.local>
2016-06-08 14:23:18 -0400

    Copied Eric's Plotting Method and Added two more JAR Files

--- src/main/LinePlot.java
+++ src/main/LinePlot.java
@@ -12,10 +12,52 @@ import org.jfree.chart.ChartFrame;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
 
 public class LinePlot {
 		
+	/**
+	 * Converts the roleMap into a format readable by the plotting method
+	 */
+	public void createPointLists(HashMap<String, Role> roleMap){
 
+		//Iterate through Roles in the role map
+		for (String roleName : roleMap.keySet()){
+			System.out.println("Role: " + roleName);
+
+			Role role = roleMap.get(roleName);
+			HashMap<String, ProjectType> projectTypeMap = role.getPmap();
+
+			//Iterate through ProjectTypes in this role
+			for (String projectCode : projectTypeMap.keySet()){
+				System.out.println("    Project Code: " + projectCode);
+
+				//Scaling the map for the number of employees
+				double employeeNumber = role.getEmp_Set().size();
+				HashMap<Date, Double> weekMapEmployees = projectTypeMap.get(projectCode).scaleMap(employeeNumber);
+
+				//Goes through the weeks -> hours and builds up the points into a list
+				for (Date weekDate : weekMapEmployees.keySet()){
+					double employeeHours = weekMapEmployees.get(weekDate);
+
+					Calendar cal = Calendar.getInstance();
+					cal.setTime(weekDate);
+					int year = cal.get(Calendar.YEAR);
+					int month = cal.get(Calendar.MONTH);
+					int day = cal.get(Calendar.DAY_OF_MONTH);
+
+					System.out.print("        Date: " + day + " " + month + " " + year);
+					System.out.println(" Employees: " + employeeHours);
+
+					//Omair: Do the plotting here
+					//X = Day, month, years
+					//Y = employeeHours
+				}
+			}
+		}
+	}
 
 
 	public static void main(String[] args) {
